9a274e008bfde62b9e67418b48bfc727d890b075
lalogiquefloue <lalogiquefloue@gmail.com>
2024-10-09 21:19:37 -0400

    revert breaking changes following the parametrization of the test

--- src/test/java/com/marginallyclever/makelangelo/makeart/imagefilter/FilterCMYKTest.java
+++ src/test/java/com/marginallyclever/makelangelo/makeart/imagefilter/FilterCMYKTest.java
@@ -9,15 +9,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
 
-import java.util.stream.Stream;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 public class FilterCMYKTest {
@@ -39,11 +37,12 @@ public class FilterCMYKTest {
     }
 
     // NEW TEST
-    @ParameterizedTest
-    @MethodSource("testColors")
-    public void testFilter(int colorRGB, int[] expectedCMYK) throws IOException {
+    @Test
+    public void testFilter() {
 
         BufferedImage bufferedImage;
+        int colorRGB;
+        int[] colorCMYK;
         int expectedCyan;
         int expectedMagenta;
         int expectedYellow;
@@ -53,44 +52,47 @@ public class FilterCMYKTest {
         int realYellow;
         int realBlack;
 
-        expectedCyan    = expectedCMYK[0];
-        expectedMagenta = expectedCMYK[1];
-        expectedYellow  = expectedCMYK[2];
-        expectedBlack   = expectedCMYK[3];
+        List<SimpleEntry<Integer, int[]>> testColors = new ArrayList<>(Arrays.asList(   // rgb -> CMYK -> inverted CMYK
+                new SimpleEntry<>(Color.BLACK.getRGB(), new int[]{255, 255, 255, 0}),   // (0,0,0) -> (0,0,0,255) -> (255,255,255,0)
+                new SimpleEntry<>(Color.WHITE.getRGB(), new int[]{255, 255, 255, 255}), // (255,255,255) -> (0,0,0,0) -> (255,255,255,255)
+                new SimpleEntry<>(Color.BLUE.getRGB(), new int[]{0, 0, 255, 255}),      // (0,0,255) -> (255,255,0,0) -> (0,0,255,255)
+                new SimpleEntry<>(Color.RED.getRGB(), new int[]{255, 0, 0, 255}),       // (255,0,0) -> (0,255,255,0) -> (255,0,0,255)
+                new SimpleEntry<>(Color.YELLOW.getRGB(), new int[]{255, 255, 0, 255}),  // (255,255,0) -> (0,0,255,0) -> (255,255,0,255)
+                new SimpleEntry<>(Color.GREEN.getRGB(), new int[]{0, 255, 0, 255}),     // (0,255,0) -> (255,0,255,0) -> (0,255,0,255)
+                new SimpleEntry<>(Color.CYAN.getRGB(), new int[]{0, 255, 255, 255}),    // (0,255,255) -> (255,0,0,0) -> (0,255,255,255)
+                new SimpleEntry<>(Color.MAGENTA.getRGB(), new int[]{255, 0, 255, 255})  // (255,0,255) -> (0,255,0,0) -> (255,0,255,255)
+        ));
 
-        bufferedImage = new BufferedImage(1,1, BufferedImage.TYPE_INT_RGB);
-        bufferedImage.setRGB(0, 0, colorRGB);
+        for (SimpleEntry<Integer, int[]> entry : testColors) {
+            colorRGB = entry.getKey();
+            colorCMYK = entry.getValue();
 
-        TransformedImage img = new TransformedImage(bufferedImage);
-        FilterCMYK filter = new FilterCMYK(img);
-        filter.filter();
+            expectedCyan    = colorCMYK[0];
+            expectedMagenta = colorCMYK[1];
+            expectedYellow  = colorCMYK[2];
+            expectedBlack   = colorCMYK[3];
 
-        TransformedImage channelCyan    = filter.getC();
-        TransformedImage channelMagenta = filter.getM();
-        TransformedImage channelYellow  = filter.getY();
-        TransformedImage channelBlack   = filter.getK();
+            bufferedImage = new BufferedImage(1,1, BufferedImage.TYPE_INT_RGB);
+            bufferedImage.setRGB(0, 0, colorRGB);
 
-        realCyan    = channelCyan.sample1x1(0,0);
-        realMagenta = channelMagenta.sample1x1(0,0);
-        realYellow  = channelYellow.sample1x1(0,0);
-        realBlack   = channelBlack.sample1x1(0,0);
+            TransformedImage img = new TransformedImage(bufferedImage);
+            FilterCMYK filter = new FilterCMYK(img);
+            filter.filter();
 
-        assertEquals(expectedCyan, realCyan);
-        assertEquals(expectedMagenta, realMagenta);
-        assertEquals(expectedYellow, realYellow);
-        assertEquals(expectedBlack, realBlack);
-    }
+            TransformedImage channelCyan    = filter.getC();
+            TransformedImage channelMagenta = filter.getM();
+            TransformedImage channelYellow  = filter.getY();
+            TransformedImage channelBlack   = filter.getK();
 
-    static Stream<Arguments> testColors() {
-        return Stream.of(
-                arguments(Color.BLACK.getRGB(), new int[]{255, 255, 255, 0}),
-                arguments(Color.WHITE.getRGB(), new int[]{255, 255, 255, 255}),
-                arguments(Color.BLUE.getRGB(), new int[]{0, 0, 255, 255}),
-                arguments(Color.RED.getRGB(), new int[]{255, 0, 0, 255}),
-                arguments(Color.YELLOW.getRGB(), new int[]{255, 255, 0, 255}),
-                arguments(Color.GREEN.getRGB(), new int[]{0, 255, 0, 255}),
-                arguments(Color.CYAN.getRGB(), new int[]{0, 255, 255, 255}),
-                arguments(Color.MAGENTA.getRGB(), new int[]{255, 0, 255, 255})
-        );
+            realCyan    = channelCyan.sample1x1(0,0);
+            realMagenta = channelMagenta.sample1x1(0,0);
+            realYellow  = channelYellow.sample1x1(0,0);
+            realBlack   = channelBlack.sample1x1(0,0);
+
+            assertEquals(expectedCyan, realCyan);
+            assertEquals(expectedMagenta, realMagenta);
+            assertEquals(expectedYellow, realYellow);
+            assertEquals(expectedBlack, realBlack);
+        }
     }
 }
